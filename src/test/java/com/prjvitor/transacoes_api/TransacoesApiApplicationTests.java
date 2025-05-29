2c877dc3aed6e755388becf4700bb86670d8c9b2
vitorh07 <vitor07devsenai@gmail.com>
2025-05-28 23:50:40 -0300

    initial commit

---
+++ src/test/java/com/prjvitor/transacoes_api/TransacoesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prjvitor.transacoes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
